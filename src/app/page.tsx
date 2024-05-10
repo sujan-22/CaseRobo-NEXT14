bd26ccb7569e61fe7ac67575fd2754bbfcf530ef
Sujan <sujanrokad44@gmail.com>
2024-05-10 02:59:46 -0400

    initialize project; hero page almost done; needs animated section done; implement user authentication using Kinde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,177 @@
+import { Icons } from "@/components/Icons";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Phone from "@/components/Phone";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    return (
+        <div className="bg-slate-50">
+            <section>
+                <MaxWidthWrapper className=" pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+                    <div className=" col-span-2 px-6 lg:px-0 lg:pt-4">
+                        <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+                            <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                                <Image
+                                    src="/snake-1.png"
+                                    alt=""
+                                    width={100}
+                                    height={100}
+                                    className=" w-full"
+                                />
+                            </div>
+                            <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                                Your Image on a{" "}
+                                <span className="bg-blue-600 px-2 text-white">
+                                    Custom
+                                </span>{" "}
+                                Phone Case
+                            </h1>
+                            <p className=" mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                                Capture your favourite memories with your own,{" "}
+                                <span className=" font-semibold">
+                                    one-of-one
+                                </span>{" "}
+                                phone case. CaseRobo allows you to protect your
+                                memories, not just your phone case.
+                            </p>
+                            <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                                <div className="space-y-2">
+                                    <li className="flex gap-1.5 items-center text-left">
+                                        <Check className="h-5 w-5 shrink-0 text-blue-600" />
+                                        High-quality, durable material
+                                    </li>
+                                    <li className="flex gap-1.5 items-center text-left">
+                                        <Check className="h-5 w-5 shrink-0 text-blue-600" />
+                                        5 year print quarantee
+                                    </li>
+                                    <li className="flex gap-1.5 items-center text-left">
+                                        <Check className="h-5 w-5 shrink-0 text-blue-600" />
+                                        Modern iPhone models supported
+                                    </li>
+                                </div>
+                            </ul>
+                            <div className=" mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                                <div className="flex -space-x-4">
+                                    <Image
+                                        className="inline-block rounded-full ring-2 ring-slate-100"
+                                        src="/users/user-1.png"
+                                        alt="user"
+                                        width={50}
+                                        height={50}
+                                    />
+                                    <Image
+                                        className="inline-block rounded-full ring-2 ring-slate-100"
+                                        src="/users/user-2.png"
+                                        alt="user"
+                                        width={50}
+                                        height={50}
+                                    />
+                                    <Image
+                                        className="inline-block rounded-full ring-2 ring-slate-100"
+                                        src="/users/user-3.png"
+                                        alt="user"
+                                        width={50}
+                                        height={50}
+                                    />
+                                    <Image
+                                        className="inline-block rounded-full ring-2 ring-slate-100"
+                                        src="/users/user-4.jpg"
+                                        alt="user"
+                                        width={50}
+                                        height={50}
+                                    />
+                                    <Image
+                                        className="inline-block rounded-full object-cover ring-2 ring-slate-100"
+                                        src="/users/user-5.jpg"
+                                        alt="user"
+                                        width={50}
+                                        height={50}
+                                    />
+                                </div>
+                                <div className="flex flex-col justify-between items-center sm:items-start ">
+                                    <div className="flex gap-0.5">
+                                        <Star className=" h-4 w-4 text-blue-600 fill-blue-600" />
+                                        <Star className=" h-4 w-4 text-blue-600 fill-blue-600" />
+                                        <Star className=" h-4 w-4 text-blue-600 fill-blue-600" />
+                                        <Star className=" h-4 w-4 text-blue-600 fill-blue-600" />
+                                        <Star className=" h-4 w-4 text-blue-600 fill-blue-600" />
+                                    </div>
+                                    <p>
+                                        <span className="font-semibold">
+                                            2.234
+                                        </span>{" "}
+                                        happy customers
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+                        <div className="relative md:max-w-xl">
+                            <img
+                                src="/your-image.png"
+                                alt=""
+                                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+                            />
+                            <img
+                                src="/line.png"
+                                alt=""
+                                className="absolute w-20 -left-6 -bottom-6 select-none"
+                            />
+                            <Phone
+                                className="w-64"
+                                imgSrc="/testimonials/1.jpg"
+                            />
+                        </div>
+                    </div>
+                </MaxWidthWrapper>
+            </section>
+
+            {/* animated landing page section */}
+            <section className=" bg-slate-100 py-24">
+                <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+                    <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+                        <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                            What our{" "}
+                            <span className="relative px-2">
+                                customers{" "}
+                                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-blue-600" />
+                            </span>{" "}
+                            say
+                        </h2>
+                        <img
+                            src="/snake-2.png"
+                            className=" w-24 order-0 lg:order-2"
+                            alt=""
+                        />
+                    </div>
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+                        <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                            <div className="flex gap-0.5 mb-2">
+                                <Star className=" h-5 w-5 text-blue-600 fill-blue-600" />
+                                <Star className=" h-5 w-5 text-blue-600 fill-blue-600" />
+                                <Star className=" h-5 w-5 text-blue-600 fill-blue-600" />
+                                <Star className=" h-5 w-5 text-blue-600 fill-blue-600" />
+                                <Star className=" h-5 w-5 text-blue-600 fill-blue-600" />
+                            </div>
+                            <div className="text-lg leading-8">
+                                <p>
+                                    The case feels durable and I even got a
+                                    compliment on the design. Had the case for
+                                    two and a half months now and{" "}
+                                    <span className=" p-0.5 bg-slate-800 text-white">
+                                        the image is super clear
+                                    </span>
+                                    , on the case I had before, the immage
+                                    started fading into yellow-ish color after a
+                                    couple weeks. Love it.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </MaxWidthWrapper>
+            </section>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
